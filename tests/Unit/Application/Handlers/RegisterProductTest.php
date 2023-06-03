09f8b38b34507b020cbf804ea4b3f10076f095f5
Mateus Macedo Dos Anjos <mateus.anjos@fretebras.com.br>
2023-06-03 18:45:38 -0300

    test(products): :white_check_mark: ensure register new products
    - assert result is success
    - assert product is equal to repository return value
    - assert application error is return when factory cannot create product
    - assert repository error is return when repository cannot save product

---
+++ tests/Unit/Application/Handlers/RegisterProductTest.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Application\Handlers;
+
+use App\Application\Commands\RegisterProduct as Command;
+use App\Application\Handlers\RegisterProduct as Handler;
+use App\Domain\ProductFactory;
+use App\Domain\{Product, ProductRepository};
+use Frete\Core\Application\Errors\ApplicationError;
+use Frete\Core\Application\IDispatcher;
+use Frete\Core\Infrastructure\Database\Errors\RepositoryError;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class RegisterProductTest extends TestCase
+{
+    private Command|MockObject $command;
+    private Product|MockObject $product;
+    private ProductFactory|MockObject $productFactoryMock;
+    private ProductRepository|MockObject $productRepositoryMock;
+    private IDispatcher|MockObject $dispatcherMock;
+    private Handler $sut;
+
+    protected function setUp(): void
+    {
+        $this->productFactoryMock = $this->createMock(ProductFactory::class);
+        $this->productRepositoryMock = $this->createMock(ProductRepository::class);
+        $this->dispatcherMock = $this->createMock(IDispatcher::class);
+
+        $this->sut = new Handler(
+            $this->productFactoryMock,
+            $this->productRepositoryMock,
+            $this->dispatcherMock
+        );
+
+        $this->command = $this->createStub(Command::class);
+
+        $this->product = $this->createStub(Product::class);
+    }
+
+    /**
+     * @test
+     */
+    public function handle_result_as_success_with_product()
+    {
+        $this->productFactoryMock
+            ->expects($this->once())
+            ->method('create')
+            ->with($this->command)
+            ->willReturn($this->product);
+        $this->productRepositoryMock
+            ->expects($this->once())
+            ->method('save')
+            ->with($this->product)
+            ->willReturn($this->product);
+        $this->dispatcherMock
+            ->expects($this->once())
+            ->method('dispatchContextEvents')
+            ->with($this->product);
+
+        $result = $this->sut->handle($this->command);
+
+        $this->assertTrue($result->isSuccess());
+        $this->assertSame($this->product, $result->getValue());
+    }
+
+    /**
+     * @test
+     */
+    public function handle_result_as_failure_with_application_error()
+    {
+        $this->productFactoryMock
+            ->expects($this->once())
+            ->method('create')
+            ->with($this->command)
+            ->willReturn(null);
+
+        $result = $this->sut->handle($this->command);
+
+        $this->assertTrue($result->isFailure());
+        $this->assertInstanceOf(ApplicationError::class, $result->getError());
+    }
+
+    /**
+     * @test
+     */
+    public function handle_result_as_failure_with_repository_error(): void
+    {
+        $this->productFactoryMock
+            ->expects($this->once())
+            ->method('create')
+            ->with($this->command)
+            ->willReturn($this->product);
+        $this->productRepositoryMock
+            ->expects($this->once())
+            ->method('save')
+            ->with($this->product)
+            ->willReturn(new RepositoryError('error'));
+
+        $result = $this->sut->handle($this->command);
+
+        $this->assertTrue($result->isFailure());
+        $this->assertInstanceOf(RepositoryError::class, $result->getError());
+    }
+}
