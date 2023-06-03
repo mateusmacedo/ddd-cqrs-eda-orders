c558a161b7f884cb1cfd7f13dda9d5c9335d35a9
Mateus Macedo Dos Anjos <mateus.anjos@fretebras.com.br>
2023-06-03 20:16:38 -0300

    feat(products): :sparkles: implement fetch product query and handler
    - this commit introduces fetchproduct query and handler.
    - the query receives a product id and fetches the corresponding product from the repository using the handler.
    - if there is an error, returns a failure result with the error.

---
+++ src/Application/Handlers/FetchProduct.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Handlers;
+
+use App\Application\Queries\FetchProduct as FetchProductQuery;
+use App\Domain\{Product, ProductRepository};
+use Frete\Core\Application\Errors\ApplicationError;
+use Frete\Core\Application\IHandler;
+use Frete\Core\Domain\Message;
+use Frete\Core\Infrastructure\Database\Errors\RepositoryError;
+use Frete\Core\Shared\Result;
+
+class FetchProduct implements IHandler
+{
+    public function __construct(
+        private readonly ProductRepository $productRepository
+    ) {
+    }
+
+    /**
+     * @param FetchProductQuery $query
+     *
+     * @return Result
+     */
+    public function handle(Message $query): Result
+    {
+        if ($query instanceof FetchProductQuery) {
+            /** @var Product|RepositoryError */
+            $result = $this->productRepository->get($query->productId);
+
+            if ($result instanceof RepositoryError) {
+                return Result::failure($result);
+            }
+
+            return Result::success($result);
+        }
+
+        return Result::failure(new ApplicationError('Invalid query type'));
+    }
+}
