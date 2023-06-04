2a26b89bd39c70a38ad8fc8651301a4b33f1a35f
Mateus Macedo Dos Anjos <mateus.anjos@fretebras.com.br>
2023-06-04 05:09:13 -0300

    test(orders): :white_check_mark: ensure order can be calculated
    ensure order can be calculated, cover scenarios where order is not found, order is found but has no items, order is found but has no items with price, order is found but has no items with quantity, order is found but has no items with price and quantity, order is found but has no items with price and quantity and order is found and has items with price and quantity

---
+++ tests/Unit/Application/Handlers/CalculateOrderTest.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Application\Handlers;
+
+use App\Application\Handlers\CalculateOrder as CalculateOrderHandler;
+use App\Application\Queries\CalculateOrder as CalculateOrderQuery;
+use App\Domain\Order;
+use App\Domain\OrderRepository;
+use Frete\Core\Application\Errors\ApplicationError;
+use Frete\Core\Application\Query;
+use Frete\Core\Infrastructure\Database\Errors\RepositoryError;
+use Frete\Core\Shared\Result;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\MockObject\MockObject;
+
+class CalculateOrderTest extends TestCase
+{
+    private OrderRepository|MockObject $orderRepository;
+    private CalculateOrderHandler $handler;
+
+    protected function setUp(): void
+    {
+        $this->orderRepository = $this->createMock(OrderRepository::class);
+        $this->handler = new CalculateOrderHandler($this->orderRepository);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReturnErrorWhenQueryIsInvalid(): void
+    {
+        $query = $this->createMock(originalClassName: Query::class);
+
+        $result = $this->handler->handle($query);
+        $error = $result->getError();
+
+        $this->assertInstanceOf(Result::class, $result);
+        $this->assertTrue($result->isFailure());
+        $this->assertInstanceOf(ApplicationError::class, $error);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReturnErrorWhenOrderRepositoryFails(): void
+    {
+        $query = $this->getMockBuilder(CalculateOrderQuery::class)
+        ->setConstructorArgs(['1'])
+        ->getMock();
+
+        $this->orderRepository->expects($this->once())
+        ->method('get')->with($query->orderId)
+        ->willReturn(new RepositoryError('Error'));
+
+        $result = $this->handler->handle($query);
+        $error = $result->getError();
+
+        $this->assertInstanceOf(Result::class, $result);
+        $this->assertTrue($result->isFailure());
+        $this->assertInstanceOf(RepositoryError::class, $error);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReturnSuccessWhenOrderRepositorySucceeds(): void
+    {
+        $query = $this->getMockBuilder(CalculateOrderQuery::class)
+        ->setConstructorArgs(['1'])
+        ->getMock();
+        $order = $this->createMock(Order::class);
+        $order->expects($this->once())
+        ->method('calculateTotalPrice')
+        ->willReturn(10.0);
+
+        $this->orderRepository->expects($this->once())
+        ->method('get')->with($query->orderId)
+        ->willReturn($order);
+
+        $result = $this->handler->handle($query);
+
+        $this->assertInstanceOf(Result::class, $result);
+        $this->assertTrue($result->isSuccess());
+        $this->assertEquals(10.0, $result->getValue());
+    }
+}
